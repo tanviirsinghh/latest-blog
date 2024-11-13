65e119545aff49e9b24e787660716414667ff69f
iTanvirSingh <71079808+tanviirsinghh@users.noreply.github.com>
2024-11-13 15:28:14 +0530

    khlara peya hoeya aa

--- src/components/ImageUpload.tsx
+++ src/components/ImageUpload.tsx
@@ -1,38 +1,38 @@
 import axios from 'axios'
 import { CLOUDINARY_URL } from '../config'
-import { useState } from 'react'
+import { PropsWithChildren, useState } from 'react'
 import { toast } from 'react-toastify'
 
 export default function ImageUpload () {
   const [image, setImage] = useState<File | null>(null)
-  const [url, setUrl] = useState('')
+  // const [url, setUrl] = useState('')
 
-  const uploadImage = async () => {
-    if (!image) {
-      console.log('No image selected')
-      toast.error('Upload image')
-      return
-    }
-    const data = new FormData()
-    data.append('file', image)
-    data.append('upload_preset', 'Blog-Project')
-    data.append('cloud_name', 'dktr9buob')
-    console.log('start request')
-    try {
-      const response = await axios.post(`${CLOUDINARY_URL}`, data, {
-        headers: {
-          'Content-Type': 'multipart/form-data'
-        }
-      })
-      console.log(response.data.secure_url)
-      console.log('complete')
-      setUrl(response.data.secure_url)
-    } catch (e) {
-      return Response.json({
-        msg: "Image didn't upload"
-      })
-    }
-  }
+  // const uploadImage = async () => {
+  //   if (!image) {
+  //     console.log('No image selected')
+  //     toast.error('Upload image')
+  //     return
+  //   }
+  //   const data = new FormData()
+  //   data.append('file', image)
+  //   data.append('upload_preset', 'Blog-Project')
+  //   data.append('cloud_name', 'dktr9buob')
+  //   console.log('start request')
+  //   try {
+  //     const response = await axios.post(`${CLOUDINARY_URL}`, data, {
+  //       headers: {
+  //         'Content-Type': 'multipart/form-data'
+  //       }
+  //     })
+  //     console.log(response.data.secure_url)
+  //     console.log('complete')
+  //     setUrl(response.data.secure_url)
+  //   } catch (e) {
+  //     return Response.json({
+  //       msg: "Image didn't upload"
+  //     })
+  //   }
+  // }
 
   const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const fileInput = event.target.files
--- src/pages/TextEditor.tsx
+++ src/pages/TextEditor.tsx
@@ -9,82 +9,128 @@ import { useNavigate } from 'react-router-dom'
 import Navbar from '../components/Navbar'
 import { toast } from 'react-toastify'
 // import { Toast } from 'node_modules/react-toastify/dist/components'
-import ImageUpload from '../components/ImageUpload';
+import ImageUpload from '../components/ImageUpload'
 
 export default function TextEditor () {
   const editorRef = useRef<TinyMCEEditor | null>(null)
   const navigate = useNavigate()
 
   const [title, setTitle] = useState('')
-  const [descript, setDescript] = useState("" )
-  // const [image, setImage] = useState<File | null>(null)
+  const [descript, setDescript] = useState('')
+  const [image, setImage] = useState<File | null>(null)
 
-  // const [url, setUrl] = useState("")
-
+  const [url, setUrl] = useState("")
 
   const SendPost = async () => {
-
-    if ( !title && !descript) {
-      toast.error('Title and Description cannot be empty');
-      return;
+    if (!title && !descript) {
+      toast.error('Title and Description cannot be empty')
+      return
     }
     // uploading the img to the cloudinary server
-      // if (!image) {
-      //   console.log('No image selected')
-      //   toast.error('Upload image')
-      //   return
-      // }
-      // const data = new FormData()
-      // data.append('file', image)
-      // data.append('upload_preset', 'Blog-Project')
-      // data.append('cloud_name', 'dktr9buob')
-      // console.log('start request')
-      // try {
-      //   const response = await axios.post(`${CLOUDINARY_URL}`, data, {
-      //     headers: {
-      //       'Content-Type': 'multipart/form-data'
-      //     }
-      //   })
-      //   console.log(response.data.secure_url)
-      //   console.log('complete')
-      //   setUrl(response.data.secure_url)
-      // } catch (e) {
-      //   return Response.json({
-      //     msg: "Image didn't upload"
-      //   })
-      // }
+ 
+     // Append if file is not null
+
+    try {
+    if (image) {
+      console.log('No image selected')
+      // toast.error('Upload image')
     
-    try{
-      console.log("entered request frontend")
-    const response = await axios.post(
-      `${BACKEND_URL}/api/v1/blog`,
-      {
-        title,
-        content: descript
-        // img:img file
-      },
-      {
+    
+    const data = new FormData()
+    data.append('file', image)
+    data.append('upload_preset', 'Blog-Project')
+    data.append('cloud_name', 'dktr9buob')
+    console.log('start request')
+    try {
+      const response = await axios.post(`${CLOUDINARY_URL}`, data, {
         headers: {
-          Authorization: localStorage.getItem('token')
+          'Content-Type': 'multipart/form-data'
         }
+      })
+      console.log(response.data.secure_url)
+      console.log('complete')
+      setUrl(response.data.secure_url)
+    } catch (e) {
+      toast.error("Error Occurred / Please Re-Upload")
+      return Response.json({
+      
+        msg: "Image didn't upload",
+      })
+    }
+    }
+      console.log('entered request frontend')
+      const response = await axios.post(
+        `${BACKEND_URL}/api/v1/blog`,
+        {
+          title,
+          content: descript,
+          url: url || ""
+        
+        },
+        {
+          headers: {
+            Authorization: localStorage.getItem('token'),
+           
+          }
+        }
+      )
+      navigate(`/blog/${response.data.id}`)
+    } catch (e: unknown) {
+      if (axios.isAxiosError(e) && e.response?.status === 401) {
+        toast.error('User not found / Sign up first')
+      } else if (axios.isAxiosError(e) && e.response?.status === 500) {
+        toast.error('Please try again / Internal Server Error')
+      } else if (axios.isAxiosError(e) && e.response?.status === 411) {
+        toast.error('Input Not Correct')
+      } else if (axios.isAxiosError(e) && e.response?.status === 403) {
+        toast.error('Email Already in use')
       }
-    )
-    navigate(`/blog/${response.data.id}`)
+    }
   
-  }catch (e : unknown ){
-    if(axios.isAxiosError(e) && e.response?.status === 401){
-   toast.error('User not found / Sign up first')
-} else if (axios.isAxiosError(e) && e.response?.status === 500){
-    toast.error('Please try again / Internal Server Error')
+ 
+
+
+//       if file doesnot exist
+// try {
+//   console.log('entered request with no file frontend')
+//   const response = await axios.post(
+//     `${BACKEND_URL}/api/v1/blog`,
+//     {
+//       title,
+//       content: descript,
+      
+//     },
+//     {
+//       headers: {
+//         Authorization: localStorage.getItem('token'),
+       
+//       }
+//     }
+//   )
+//   navigate(`/blog/${response.data.id}`)
+// } catch (e: unknown) {
+//   if (axios.isAxiosError(e)) {
+//     switch (e.response?.status) {
+//       case 401:
+//         toast.error('User not found / Sign up first');
+//         break;
+//       case 500:
+//         toast.error('Please try again / Internal Server Error');
+//         break;
+//       case 411:
+//         toast.error('Input Not Correct');
+//         break;
+//       case 403:
+//         toast.error('Email Already in use');
+//         break;
+//       default:
+//         toast.error('An unexpected error occurred');
+//     }
+//   }
+// }
 }
-else if (axios.isAxiosError(e) && e.response?.status === 411){
-    toast.error('Input Not Correct')
-}
-else if (axios.isAxiosError(e) && e.response?.status === 403){
-    toast.error('Email Already in use')
-}
- }
-  }
+
+
   //   const log = () => {
   //     if (editorRef.current) {
   //       console.log(editorRef.current.getContent());
@@ -95,12 +141,11 @@ else if (axios.isAxiosError(e) && e.response?.status === 403){
     <div className='h-screen w-full  flex-col justify-center items-center'>
       <Navbar />
       <div className='w-full h-44 bg-pink-700 flex justify-center items-end'>
-       {/* <div className='bg-violet-500 w-2/4 flex justify-center items-center'>
-          <ImageUpload/>
-        </div> */}
+        <div className='bg-violet-500 w-2/4 flex justify-center items-center'>
+          <ImageUpload />
         </div>
+      </div>
       <div className='flex bg-green-900  justify-center items-center h-32 w-full  '>
-       
         <div className=' bg-yellow-500 max-w-screen-lg h-16 w-2/3 flex'>
           <input
             onChange={e => {
@@ -117,10 +162,9 @@ else if (axios.isAxiosError(e) && e.response?.status === 403){
           apiKey='s73wk7i6fwr11ew23vudqfd79tjyajof2inq6b6qg8a9c9x6
 '
           onInit={(_evt, editor) => (editorRef.current = editor)}
-          
           init={{
             height: 500,
-            width:1000,
+            width: 1000,
             menubar: true,
             plugins: [
               'advlist',
@@ -149,43 +193,12 @@ else if (axios.isAxiosError(e) && e.response?.status === 403){
             content_style:
               'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
           }}
-          onEditorChange={(content) => setDescript(content)} // Update state on change
-      
+          onEditorChange={content => setDescript(content)} // Update state on change
         />
       </div>
       <div className='w-full h-inherit  flex justify-center items-center h-24 '>
         <button
-          onClick={async () => {
-            
-        
-
-            if (title &&  descript == " ") {
-              toast.error('Title and Description cannot be empty');
-              return;
-            }
-
-            try{
-              
-            const response = await axios.post(
-              `${BACKEND_URL}/api/v1/blog`,
-              {
-                title,
-                content: descript
-              },
-              {
-                headers: {
-                  Authorization: localStorage.getItem('token')
-                }
-              }
-            )
-            navigate(`/blog/${response.data.id}`)
-          
-          }catch(e){
-            toast.error('Inputs cannot be empty')
-            return ('fcuk')
-            
-          }
-          }}
+          onClick={SendPost}
           type='submit'
           className=' inline-flex items-center px-7 py-3 text-base font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-blue-800'
         >
@@ -195,3 +208,31 @@ else if (axios.isAxiosError(e) && e.response?.status === 403){
     </div>
   )
 }
+
+// if (title &&  descript == " ") {
+//   toast.error('Title and Description cannot be empty');
+//   return;
+// }
+
+// try{
+
+// const response = await axios.post(
+//   `${BACKEND_URL}/api/v1/blog`,
+//   {
+//     title,
+//     content: descript,
+//     // img:img
+//   },
+//   {
+//     headers: {
+//       Authorization: localStorage.getItem('token')
+//     }
+//   }
+// )
+// navigate(`/blog/${response.data.id}`)
+
+// }catch(e){
+// toast.error('Inputs cannot be empty')
+// return ('fcuk')
+
+// }
