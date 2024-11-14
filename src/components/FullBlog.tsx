402718dcec526eaf297f2953c5a43562ffb14e77
iTanvirSingh <71079808+tanviirsinghh@users.noreply.github.com>
2024-11-14 15:05:32 +0530

    changed the FullBlog.tsx file

--- src/components/FullBlog.tsx
+++ src/components/FullBlog.tsx
@@ -1,64 +1,57 @@
+import { Blog } from '../hooks/index'
+import { Avatar } from './BlogCard'
+import DOMPurify from 'dompurify'
+import { useNavigate } from 'react-router-dom'
+import Navbar from './Navbar'
 
-import { Appbar } from "./Appbar"
-// import { Avatar } from "./BlogCard"
-// import { Blog } from '../pages/Blog';
-import { Blog } from '../hooks/index';
-import { Avatar } from "./BlogCard";
-import DOMPurify from "dompurify";
-import { useNavigate } from "react-router-dom";
+export const FullBlog = ({ blog }: { blog: Blog }) => {
+  const navigate = useNavigate()
 
-export const FullBlog =( { blog  }: {blog:Blog}) => {
-    const navigate = useNavigate()
- 
-       
-    if(!localStorage.getItem("token")){
-        navigate('/signin')
-        return
-     
-   }
-    return <div>
-        <Appbar />
-        <div className="flex justify-center">
-            <div className="grid grid-cols-12 px-10 w-full pt-200 max-w-screen-xl pt-12">
-                <div className="col-span-9">
-                    <div className="h-[23rem] w-2/4 bg-pink-700 ">
-                    <img className="h-full w-full" src={blog.url}/>
-                     
-                    </div>
-                    <div className=" bg-green-700 text-4xl font-extrabold break-words">
-                        {blog.title}
-                    </div>
-                    <div className="text-slate-500 pt-2 ">
-                        Post on 2nd December 2023
-                    </div>
-                    <div className="pt-4 flex flex-col break-words" dangerouslySetInnerHTML={{ 
-          __html: DOMPurify.sanitize(blog.content) 
-        }}  >
-                      
-                        
-                    </div>
-                </div>
-                <div className="col-span-3  ml-8">
-        
-                    <div className="text-slate-600 text-lg">
-                        Author
-                    </div>
-                    <div className="flex w-full">
-                        <div className="pr-4 flex flex-col justify-center">
-                            <Avatar size="big" name={blog.author.name || "Anonymous"} />
-                        </div>
-                        <div>
-                            <div className="text-xl font-bold">
-                                {blog.author.name || "Anonymous"}
-                            </div>
-                            <div className="pt-2 text-slate-500">
-                                Random catch phrase about the author's ability to grab the user's attention
-                            </div>
-                        </div>
-                    </div>  
-                </div>
-                
+  if (!localStorage.getItem('token')) {
+    navigate('/signin')
+    return
+  }
+  return (
+    <div>
+      <Navbar />
+      <div className='flex justify-center'>
+        <div className='grid grid-cols-12 px-10 w-full pt-200 max-w-screen-xl pt-12'>
+          <div className='col-span-9'>
+            <div className='h-[23rem] w-2/4 bg-pink-700 '>
+              <img className='h-full w-full' src={blog.url} />
             </div>
+            <div className=' bg-green-700 text-4xl font-extrabold break-words'>
+              {blog.title}
+            </div>
+            <div className='text-slate-500 pt-2 '>
+              Post on 2nd December 2023
+            </div>
+            <div
+              className='pt-4 flex flex-col break-words'
+              dangerouslySetInnerHTML={{
+                __html: DOMPurify.sanitize(blog.content)
+              }}
+            ></div>
+          </div>
+          <div className='col-span-3  ml-8'>
+            <div className='text-slate-600 text-lg'>Author</div>
+            <div className='flex w-full'>
+              <div className='pr-4 flex flex-col justify-center'>
+                <Avatar size='big' name={blog.author.name || 'Anonymous'} />
+              </div>
+              <div>
+                <div className='text-xl font-bold'>
+                  {blog.author.name || 'Anonymous'}
+                </div>
+                <div className='pt-2 text-slate-500'>
+                  Random catch phrase about the author's ability to grab the
+                  user's attention
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
+      </div>
     </div>
+  )
 }
--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -4,6 +4,7 @@ import { FullBlog } from "../components/FullBlog";
 import {  useBlog } from "../hooks";
 
 import {useNavigate, useParams} from "react-router-dom";
+import Navbar from '../components/Navbar';
 
 
 // atomFamilies/selectorFamilies
@@ -26,8 +27,7 @@ export default function  Blog  () {
        }
     if (loading || !blog) {
         return <div>
-            <Appbar />
-        
+            <Navbar/>        
             <div className="h-screen flex flex-col justify-center">
                
                 <div className="flex justify-center">
