731d8b9ab6dc56ab78e25b7d26b2511c1fbf39ed
iTanvirSingh <71079808+tanviirsinghh@users.noreply.github.com>
2024-11-25 14:45:20 +0530

    changes

--- src/pages/LatestSignUpPage.tsx
+++ src/pages/LatestSignUpPage.tsx
@@ -1,6 +1,6 @@
 import { BACKEND_URL, CLOUDINARY_URL } from '@/config'
 import axios from 'axios'
-import { FormEvent, useEffect, useState } from 'react'
+import { FormEvent, useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { toast } from 'react-toastify'
 
