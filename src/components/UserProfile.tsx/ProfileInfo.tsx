bd48720f29580165370d3963faea21bf3bd6d09a
iTanvirSingh <71079808+tanviirsinghh@users.noreply.github.com>
2024-11-27 18:44:41 +0530

    resolved error

--- src/components/UserProfile.tsx/ProfileInfo.tsx
+++ src/components/UserProfile.tsx/ProfileInfo.tsx
@@ -19,7 +19,6 @@ export function Photo({ name, size = "small" }: { name: string, size?: "small" |
 }
 export default function ProfileInfo ({user} : {user: User }) {
   // const { id } = useParams();
-
   const [isEditing, setIsEditing] = useState(false)
   const [editedUser, setEditedUser] = useState<Userinfo>({
     name: '',
