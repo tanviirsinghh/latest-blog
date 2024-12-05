bbfcf4ec74a9d694a9601dc4457f08a7110585ac
iTanvirSingh <71079808+tanviirsinghh@users.noreply.github.com>
2024-12-05 09:43:02 +0530

    changes

--- src/pages/NewFullBlog.tsx
+++ src/pages/NewFullBlog.tsx
@@ -24,7 +24,7 @@ export default function NewFullBlog({ blog }: { blog: Blog }) {
   //     });
   //     setIsBookmarked(response.data.isBookmarked);
   //   }
-  //   fetchBookmark
+  //   fetchBookmar
   
 
   const [user, setUser] = useState({
