a54e152a97643987fc4d68b5fb0b9f117d313bd6
iTanvirSingh <71079808+tanviirsinghh@users.noreply.github.com>
2024-12-09 11:28:48 +0530

    redesigned

--- src/components/Bolt-user-profile/UserProfileBolt.tsx
+++ src/components/Bolt-user-profile/UserProfileBolt.tsx
@@ -321,7 +321,7 @@ export default function ProfileInfo () {
                         {/* <label className="text-sm font-bold text-gray-500 tracking-wide">Title</label> */}
                         {/* <input
             type='file'
-            className="textorder border-gray-300 rounded-lg focus:outline-none focus:border-indigo-500"
+            className="border-gray-300 rounded-lg focus:outline-none focus:border-indigo-500"
             
                    */}
               </div>
