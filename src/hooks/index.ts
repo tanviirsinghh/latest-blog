fdf12b1a8575f89cca34bc88651520fb0d6b15e8
iTanvirSingh <71079808+tanviirsinghh@users.noreply.github.com>
2024-12-12 14:34:49 +0530

    'linting'

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -181,7 +181,7 @@ export const useBlogsPersonal = (authorId?: string) => {
     const fetchBlogs = async () => {
       console.log('user blog personal hook')
       // const queryParams = new URLSearchParams(location.search)
-      // const authorId = queryParams.get
+      // const authorId = queryParams
 
       const token = localStorage.getItem('token')
       if (!token) {
