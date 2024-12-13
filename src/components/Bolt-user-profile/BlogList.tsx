3dcc47da43cf84ddabd4e2a28434ef0649e076a3
iTanvirSingh <71079808+tanviirsinghh@users.noreply.github.com>
2024-12-13 15:33:37 +0530

    first commit

--- src/components/Bolt-user-profile/BlogList.tsx
+++ src/components/Bolt-user-profile/BlogList.tsx
@@ -1,14 +1,13 @@
+import { BlogCard } from './BlogCard'
+import { sampleBlogs } from '../../lib/blogs'
 
-import { BlogCard } from './BlogCard';
-import { sampleBlogs } from '../../lib/blogs';
-
-export function BlogList() {
+export function BlogList () {
   return (
-    <div className="grid grid-cols-1 gap-8">
+    <div className='grid grid-cols-1 gap-8'>
       {sampleBlogs.map((blog, index) => (
         <BlogCard key={index} {...blog} />
       ))}
       {/* <BlogCard/> */}
     </div>
-  );
+  )
 }
