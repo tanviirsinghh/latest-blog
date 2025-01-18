1dc335aa097a067902d473d2e4966dbc546b3e96
iTanvirSingh <71079808+tanviirsinghh@users.noreply.github.com>
2025-01-18 21:16:53 +0530

    img link changed

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 
-export const BACKEND_URL = "http://127.0.0.1:8787"
+export const BACKEND_URL = "https://blog.tanveersing559.workers.dev/"
 export const CLOUDINARY_URL = "https://api.cloudinary.com/v1_1/dktr9buob/image/upload"
