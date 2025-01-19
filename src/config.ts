227d3aef04f6676814130a4927d75c5720c43e38
iTanvirSingh <71079808+tanviirsinghh@users.noreply.github.com>
2025-01-19 14:35:21 +0530

    'commit'

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 
-export const BACKEND_URL = "https://blog.tanveersing559.workers.dev"
+export const BACKEND_URL = "https://backend.tanveersing559.workers.dev"
 export const CLOUDINARY_URL = "https://api.cloudinary.com/v1_1/dktr9buob/image/upload"
